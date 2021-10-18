0e70a8b46adc6f11a29e844b96275cd29b999c21
Adrian López <adrilv20@gmail.com>
2021-10-18 10:26:43 +0200

    Implemented logic to transition from Login to Sing in window.

--- app/src/main/java/es/murallaromana/pmdm/lopezvarelaadrianproyectopmdm/LoginActivity.kt
+++ app/src/main/java/es/murallaromana/pmdm/lopezvarelaadrianproyectopmdm/LoginActivity.kt
@@ -1,14 +1,27 @@
 package es.murallaromana.pmdm.lopezvarelaadrianproyectopmdm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import es.murallaromana.pmdm.lopezvarelaadrianproyectopmdm.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
+    private lateinit var btnRegister : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
+        btnRegister = binding.btnRegister
+        btnRegister.setOnClickListener {
+            Toast.makeText(this, "Redirigiendo a la pantalla de registro.", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this,RegisterActivity::class.java))
+        }
     }
+
 }
--- app/src/main/java/es/murallaromana/pmdm/lopezvarelaadrianproyectopmdm/RegisterActivity.kt
+++ app/src/main/java/es/murallaromana/pmdm/lopezvarelaadrianproyectopmdm/RegisterActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class RegisterActivity : AppCompatActivity() {
+    private var validInput = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
