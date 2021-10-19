7d939d80ca75f1b4025f2b6638e2a662dca01b1c
Adrian López <adrilv20@gmail.com>
2021-10-19 10:07:38 +0200

    Added basic logic to registration screen.
    Still must validate inputs and store them on the shared preferences.

--- app/src/main/java/es/murallaromana/pmdm/lopezvarelaadrianproyectopmdm/RegisterActivity.kt
+++ app/src/main/java/es/murallaromana/pmdm/lopezvarelaadrianproyectopmdm/RegisterActivity.kt
@@ -2,11 +2,30 @@ package es.murallaromana.pmdm.lopezvarelaadrianproyectopmdm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import es.murallaromana.pmdm.lopezvarelaadrianproyectopmdm.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
-    private var validInput = false
+
+    private lateinit var binding : ActivityRegisterBinding
+    private lateinit var btnSignup: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        btnSignup = binding.btnSignUp
+        btnSignup.setOnClickListener {
+            if (validateInput()) {
+                // Store info and go back to the login screen
+                Toast.makeText(this, "Successful sign up. You're being redirected to the login screen.", Toast.LENGTH_SHORT).show()
+                onBackPressed()
+            }
+        }
+    }
+
+    private fun validateInput() : Boolean {
+        return true
     }
 }
