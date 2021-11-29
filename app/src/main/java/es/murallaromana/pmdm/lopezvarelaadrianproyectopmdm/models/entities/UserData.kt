0613484416d68738f5736a1ec484a12a98ac9b91
Adrián López <adrilv20@gmail.com>
2021-11-29 12:34:14 +0100

    Improve validation code.

--- app/src/main/java/es/murallaromana/pmdm/lopezvarelaadrianproyectopmdm/models/entities/UserData.kt
+++ app/src/main/java/es/murallaromana/pmdm/lopezvarelaadrianproyectopmdm/models/entities/UserData.kt
@@ -8,53 +8,40 @@ class UserData(val name: String, val email: String, val password: String, val du
         val MIN_CHARS_PASSWORD: Int = 6
         val MAX_CHARS_PASSWORD: Int = 16
         // TODO(see how to move these strings to resource files)
-        val FIELD_NAME = "name"
+        val FIELD_NAME = "susername"
         val FIELD_EMAIL = "email"
         val FIELD_PASSWORD = "password"
         val EMPTY_FIELD_MESSAGE = "The field %s can't be empty."
         val NON_MATCHING_PASSWORDS_MESSAGE = "The introduced passwords don't match"
-        val NON_VALID_EMAIL_MESSAGE= "The introduced email isn't valid."
+        val NON_VALID_EMAIL_MESSAGE= "The given email is not valid."
         val NOT_ENOUGH_CHARS_PASSWORD_MESSAGE = "The password needs at least ${MIN_CHARS_PASSWORD} characters."
         val TOO_MANY_CHARS_PASSWORD = "The password can't have more than ${MAX_CHARS_PASSWORD} characters."
-        val EMAIL_REGEX: Regex = Regex("^[A-Z0-9+_.-]+@[A-Z0-9.-]+$")
+        val EMAIL_REGEX: Regex = Regex("^[A-ZÑa-zñ0-9+_.-]+@[a-zñ0-9.-]+$")
     }
-
-    // TODO(add custom setters to trim text inputs)
 
     private var errorMessage: String? = null
 
     fun getErrorMessage() : String? = errorMessage
 
     fun isValidData(): Boolean {
+        errorMessage = null
+
         if (name.isEmpty()) {
             errorMessage = EMPTY_FIELD_MESSAGE.format(FIELD_NAME)
-            return false
-        }
-        if (email.isEmpty()) {
+        } else if (email.isEmpty()) {
             errorMessage = EMPTY_FIELD_MESSAGE.format(FIELD_EMAIL)
-            return false
-        }
-        if (password.isEmpty()) {
-            errorMessage = EMPTY_FIELD_MESSAGE.format(FIELD_PASSWORD)
-            return false
-        }
-        if (!email.matches(EMAIL_REGEX)) {
+        } else if (!email.matches(EMAIL_REGEX)) {
             errorMessage = NON_VALID_EMAIL_MESSAGE
-            return false
-        }
-        if (password.length < MIN_CHARS_PASSWORD) {
+        } else if (password.isEmpty()) {
+            errorMessage = EMPTY_FIELD_MESSAGE.format(FIELD_PASSWORD)
+        } else if (password.length < MIN_CHARS_PASSWORD) {
             errorMessage = NOT_ENOUGH_CHARS_PASSWORD_MESSAGE
-            return false
-        }
-        if (password.length < MIN_CHARS_PASSWORD) {
+        } else if (password.length < MIN_CHARS_PASSWORD) {
             errorMessage = TOO_MANY_CHARS_PASSWORD
-            return false
-        }
-        if (password != dupPassword) {
+        } else if (password != dupPassword) {
             errorMessage = NON_MATCHING_PASSWORDS_MESSAGE
-            return false
         }
 
-        return true
+        return errorMessage == null
     }
 }
